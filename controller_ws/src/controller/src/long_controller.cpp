856afc53d22a31280071655bab7606993af6dce8
Vijay <vi4434ra-s@student.lu.se>
2026-06-25 00:26:47 +0200

    Added client server for collecting track center points

--- controller_ws/src/controller/src/long_controller.cpp
+++ controller_ws/src/controller/src/long_controller.cpp
@@ -3,24 +3,61 @@
 Controller::Controller() : rclcpp::Node("longitudinal_controller"){
 
     state_subscriber = this->create_subscription<lfs_msgs::msg::BikeState>("/current_state", 10, std::bind(&Controller::controllerCallback, this, std::placeholders::_1)); 
-    // track_subscriber = this->create_subscription<geometry_msgs::msg::Point>("/visualize_track", 10, std::bind(&Controller::trackCallback, this, std::placeholders::_1));
-
     throttle_publisher = this->create_publisher<std_msgs::msg::Float64>("/throttle_cmd", 10); 
 
+    track_client = this->create_client<track_srv::srv::ReturnTrack>("/return_track");  // Enter the service name correctly when you create a client.
+    
     RCLCPP_INFO(this->get_logger(), "Controller is active! waiting for simulation ticks..."); 
 } 
 
-// void Controller::trackCallback(const geometry_msgs::msg::Point::SharedPtr msg){
-//     latest_track_center = *msg;
-//     has_received_track = true; 
-// }
+void Controller::trackServiceRequest(){
+
+    // Wait for service to become available
+    while(!track_client->wait_for_service(1s)){
+        if (!rclcpp::ok()) {
+            RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+            return;
+        }    
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
+    }
+
+    // Create a request message
+    auto request = std::make_shared<track_srv::srv::ReturnTrack::Request>();
+
+    // Get the response using asychronous callback to prevent controller callback loop freeze. 
+    track_client->async_send_request(request,
+        [this](rclcpp::Client<track_srv::srv::ReturnTrack>::SharedFuture future) {
+            auto response = future.get();
+            
+            // Handle the response data 
+            this->latest_track_center = response->points; 
+            this->has_received_track = true;
+            
+            RCLCPP_INFO(this->get_logger(), "Successfully received track center point!");
+        }); 
+
+}
 
 void Controller::controllerCallback(const lfs_msgs::msg::BikeState::SharedPtr msg){
     
-    // if (!has_received_track) {
-    //     RCLCPP_WARN(this->get_logger(), "Waiting for first track center points...");
-    //     return;
-    // }
+    this->trackServiceRequest();
 
-    RCLCPP_INFO(this->get_logger(), "Received Bike State! Current speed: %f", msg->x_dot);
+    // Check if the controller callback has received the track center points
+    if (!has_received_track) {
+        RCLCPP_WARN(this->get_logger(), "Waiting for first track center points...");
+        return;
+    }
+
+    // Check for track center point service response 
+    RCLCPP_INFO(this->get_logger(), "Received %zu track center points!", this->latest_track_center.size());
+
+    if (!this->latest_track_center.empty()) {
+        RCLCPP_INFO(this->get_logger(), "First point -> X: %f, Y: %f, Z: %f", 
+                    this->latest_track_center[0].x, 
+                    this->latest_track_center[0].y, 
+                    this->latest_track_center[0].z);
+    } else {
+        RCLCPP_WARN(this->get_logger(), "The received track points array is empty!");
+    }
+    // RCLCPP_INFO(this->get_logger(), "Received Bike State! Current speed: %f", msg->x_dot);
 }
--- controller_ws/src/controller/src/long_controller.h
+++ controller_ws/src/controller/src/long_controller.h
@@ -5,6 +5,12 @@
 #include <rclcpp/rclcpp.hpp>
 #include <iostream> 
 #include <cmath> 
+#include <chrono>
+
+using namespace std::chrono_literals;
+
+// Service
+#include "track_srv/srv/return_track.hpp"
 
 // Messages required
 #include "lfs_msgs/msg/bike_state.hpp"
@@ -20,19 +26,20 @@ class Controller : public rclcpp::Node{
         // Load parameters
         // void loadParams();
 
+        // Service request function
+        void trackServiceRequest(); 
+
         // Controller loop callback after subscribing
         void controllerCallback(const lfs_msgs::msg::BikeState::SharedPtr msg);
-        
-        // Track center point callback
-        // void trackCallback(const geometry_msgs::msg::Point::SharedPtr msg);
 
         // Initialization of publisher and subscriber
         rclcpp::Subscription<lfs_msgs::msg::BikeState>::SharedPtr state_subscriber;
         rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr throttle_publisher; 
-        //TODO: Learn service and request in ros2 and find a way to get the data from a service for track_center points. 
-        // rclcpp::Subscription<geometry_msgs::msg::Point>::SharedPtr track_subscriber; 
+        
+        // Create a client to request the track center points from return-track service 
+        rclcpp::Client<track_srv::srv::ReturnTrack>::SharedPtr track_client; 
 
         // Initialization of variables
-        // geometry_msgs::msg::Point latest_track_center;
-        // bool has_received_track = false;
+        std::vector<geometry_msgs::msg::Point> latest_track_center; // service response is a vector of points, check return_track.cpp file for verifying. 
+        bool has_received_track = false;
 }; 
